81f17040c573e20c05c48173f8eaae181c80cafa
= <mlyon360@gmail.com>
2019-02-24 14:50:24 -0700

    Basic player profile prototype added

--- src/components/PlayerProfile.js
+++ src/components/PlayerProfile.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-const PlayerProfile = () => (
-  <div>
-    This is a player profile
-  </div>
-);
+const PlayerProfile = (props) => {
+  const playerName = props.location.pathname.slice(8);
+
+  return(
+    <div className="player-profile-container">
+      <div className="team-banner">
+      <div className="profile-picture" />
+        <div className="player-info-banner">
+          <h1>{playerName}</h1>
+          <h2>TYLER STEINKAMP</h2>
+
+          <p>ADC | Cloud9</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default PlayerProfile;
