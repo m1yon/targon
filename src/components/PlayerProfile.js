36b0aa66cac0ab222ec7d1d3bf842472d0122f15
= <mlyon360@gmail.com>
2019-02-24 15:50:31 -0700

    place holder player profile assets added

--- src/components/PlayerProfile.js
+++ src/components/PlayerProfile.js
@@ -6,13 +6,19 @@ const PlayerProfile = (props) => {
   return(
     <div className="player-profile-container">
       <div className="team-banner">
-      <div className="profile-picture" />
-        <div className="player-info-banner">
-          <h1>{playerName}</h1>
-          <h2>TYLER STEINKAMP</h2>
-
-          <p>ADC | Cloud9</p>
+        <div className="no-overflow">
+          <img src="/img/cloud9.jpg" className="team-bg" />
         </div>
+
+        <div className="overflow">
+          <img src="/img/tyler1.jpg" className="profile-picture" />
+          <div className="player-info-banner">
+            <h1>{playerName}</h1>
+            <h2>TYLER STEINKAMP</h2>
+            <p>ADC - Cloud9</p>
+          </div>
+        </div>
+
       </div>
     </div>
   );
