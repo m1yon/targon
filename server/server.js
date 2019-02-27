b620c6383d660cc1248e3acfb0a48a13abe226f5
Michael Garcia <12packer@gmail.com>
2019-02-26 19:29:37 -0700

    set up connection to mongodb

--- server/server.js
+++ server/server.js
@@ -3,6 +3,22 @@ const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
+const MongoClient = require('mongodb').MongoClient;
+
+var url = 'mongodb://heroku_4n9lqqvk:gpr0d89kotgaqj4tbko9pm66fd@ds221435.mlab.com:21435/heroku_4n9lqqvk';
+const dbName = 'heroku_4n9lqqvk';
+
+MongoClient.connect(url, function (err, client) {
+  if (err){
+    console.log("unable to connect to mongo server", err)
+  } else {
+    console.log('Connection established to', url);
+    const db = client.db(dbName);
+    const collection = db.collection('NALCS');
+    client.close();
+
+  }
+});
 
 app.use(express.static(publicPath));
 
