2eb5268e3673acbbec65e00cbbcf2c1b580e1681
Michael Lyon <mlyon360@gmail.com>
2019-02-26 20:15:19 -0700

    code optimizations

--- src/components/HomeDashboard.js
+++ src/components/HomeDashboard.js
@@ -2,9 +2,9 @@ import React from "react";
 import HomeTopBoard from "./HomeTopBoard"; 
 
 const HomeDashboard = () => (
-  <div>
+  <div className="home-dashboard">
     <h1>2019 Spring Split Leaders</h1>
-    <div className="home-dashboard">
+    <div className="home-dashboard__grid">
       <HomeTopBoard stat="kills"/>
       <HomeTopBoard stat="wins"/>
       <HomeTopBoard stat="kills"/>
