b8a4f83ddbc4ca3c3f17ef8c366d822baf87e9b0
Michael Lyon <mlyon360@gmail.com>
2019-02-27 19:57:24 -0700

    Leaderboard boilerplate setup

--- src/components/LeaderboardTable.js
+++ src/components/LeaderboardTable.js
@@ -1,9 +1,50 @@
 import React from "react";
 
-const LeaderboardTable = () => (
-  <div>
-    <p>my table</p>
-  </div>
-);
+const header = [
+  "#",
+  "Name",
+  "Kills",
+  "Deaths",
+  "Assist",
+  "KDA",
+  "KP",
+  "DTH%",
+  "FB%",
+  "GD10",
+  "XPD10",
+  "CSD10",
+  "CS%P15",
+  "DPM",
+  "DMG%",
+  "EGPM",
+  "GOLD%",
+  "WPM",
+  "WCPM"
+];
+
+const LeaderboardTable = () => {
+  let stack = [];
+  for(var i = 0; i < 10; i++)
+    stack.push(<TableEntry data={header} />);
+
+  return(
+    <div className="leaderboard-dashboard__table">
+      {stack}
+    </div>
+  );
+};
+
+const TableEntry = ({ data }) => {
+  let stack = [];
+  for(var i = 0; i < data.length; i++)
+    stack.push(<p>{data[i]}</p>);
+
+  return(
+    <div className="leaderboard-dashboard__entry">
+      {stack}
+    </div>
+  );
+};
+
 
 export default LeaderboardTable;
