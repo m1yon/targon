994bcb9a2b12dafd027c5f7c6dc84e456ed00dda
Michael Lyon <mlyon360@gmail.com>
2019-03-01 13:55:32 -0700

    homepage less cluttered, css changes

--- src/components/HomeDashboard.js
+++ src/components/HomeDashboard.js
@@ -3,22 +3,24 @@ import HomeTopBoard from "./HomeTopBoard";
 
 const HomeDashboard = () => (
   <div className="home-dashboard">
-    <h1>2019 Spring Split Leaders</h1>
-    <div className="home-dashboard__grid">
-      <HomeTopBoard stat="kills"/>
-      <HomeTopBoard stat="wins"/>
-      <HomeTopBoard stat="kills"/>
-      <HomeTopBoard stat="wins"/>
+    <div>
+      <h1>2019 Spring Split Leaders</h1>
+      <div className="home-dashboard__grid">
+        <HomeTopBoard stat="kills"/>
+        <HomeTopBoard stat="wins"/>
+        <HomeTopBoard stat="kills"/>
+        <HomeTopBoard stat="wins"/>
 
-      <HomeTopBoard stat="wins"/>
-      <HomeTopBoard stat="kills"/>
-      <HomeTopBoard stat="wins"/>
-      <HomeTopBoard stat="kills"/>
+        <HomeTopBoard stat="wins"/>
+        <HomeTopBoard stat="kills"/>
+        <HomeTopBoard stat="wins"/>
+        <HomeTopBoard stat="kills"/>
 
-      <HomeTopBoard stat="kills"/>
-      <HomeTopBoard stat="wins"/>
-      <HomeTopBoard stat="kills"/>
-      <HomeTopBoard stat="wins"/>
+        <HomeTopBoard stat="kills"/>
+        <HomeTopBoard stat="wins"/>
+        <HomeTopBoard stat="kills"/>
+        <HomeTopBoard stat="wins"/>
+      </div>
     </div>
   </div>
 );
