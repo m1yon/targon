d351a525d7cf33d92fd1444030903ab768e1253f
= <mlyon360@gmail.com>
2019-02-28 17:44:22 -0700

    basic leaderboard added

--- src/components/LeaderboardDashboard.js
+++ src/components/LeaderboardDashboard.js
@@ -1,12 +1,13 @@
 import React from "react";
 import LeaderboardTable from "./LeaderboardTable";
+import Database from "../database";
 
 const LeaderboardDashboard = (props) => {
   const statname = props.location.pathname[13].toUpperCase() + props.location.pathname.slice(14);
   
   return(<div className="leaderboard-dashboard">
     <h1>{statname} Leaderboard</h1>
-    <LeaderboardTable />
+    <LeaderboardTable data={Database["players"]} />
   </div>);
 };
 
--- src/components/LeaderboardTable.js
+++ src/components/LeaderboardTable.js
@@ -1,49 +1,57 @@
 import React from "react";
 
 const header = [
-  "#",
   "Name",
+  "Team",
   "Kills",
-  "Deaths",
-  "Assist",
-  "KDA",
-  "KP",
-  "DTH%",
-  "FB%",
-  "GD10",
-  "XPD10",
-  "CSD10",
-  "CS%P15",
-  "DPM",
-  "DMG%",
-  "EGPM",
-  "GOLD%",
-  "WPM",
-  "WCPM"
+  "Wins"
 ];
 
-const LeaderboardTable = () => {
-  let stack = [];
-  for(var i = 0; i < 10; i++)
-    stack.push(<TableEntry data={header} />);
+const LeaderboardTable = ({ data }) => {
+  const entries = data.map((obj, index) => <TableEntry key={obj["name"]} data={obj} number={index + 1}/>);
 
   return(
     <div className="leaderboard-dashboard__table">
-      {stack}
+      <TableHeader />
+      {entries}
     </div>
   );
 };
 
-const TableEntry = ({ data }) => {
-  let stack = [];
-  for(var i = 0; i < data.length; i++)
-    stack.push(<p>{data[i]}</p>);
+const TableHeader = () => {
+  const entries = [<p key={"#"}>#</p>];
+  for(let i = 0; i < 4; i++) {
+    entries.push(<p key={i}>{header[i].toUpperCase()}</p>);
+  }
 
   return(
     <div className="leaderboard-dashboard__entry">
-      {stack}
+      {entries}
     </div>
   );
+}
+
+const TableEntry = ({ data, number }) => {
+  const entries = [""];
+  entries.push(<p key={"number" + number}>{number}.</p>)
+  for(let i = 0; i < 4; i++) {
+
+    entries.push(<p key={i}>{data[header[i].toLowerCase()]}</p>);
+  }
+
+  if(number % 2 == 0) {
+    return(
+      <div className="leaderboard-dashboard__entry">
+        {entries}
+      </div>
+    );
+  } else {
+    return(
+      <div className="leaderboard-dashboard__entry leaderboard-dashboard__entry-alt">
+        {entries}
+      </div>
+    );
+  }
 };
 
 
