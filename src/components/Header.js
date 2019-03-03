825f35c5e3d5a1378cead30c793cbf7b1833a88d
Michael Lyon <mlyon360@gmail.com>
2019-03-02 19:33:32 -0700

    NavBar revamp

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,21 +3,29 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => (
   <header className="header">
-    <div className="logo-container">
-      {/* <img className="imglogo" src="/img/logo.png" /> */}
-      <h1 className="logo">Targon</h1>
+    <div className="logo__container">
+      <h1 className="header__logo">Targon</h1>
     </div>
     <div className="navbar">
-      
-      <NavLink className="navbutton" to="/" activeClassName="is-active" exact={true}>
-        <div className="navbutton">
-          Home
-        </div>
-      </NavLink>
-      
-      <NavLink className="navbutton" to="/player" activeClassName="is-active">
-          Player
-      </NavLink>
+      <div className="navbar-button__container">
+        <NavLink className="navbutton" to="/" activeClassName="is-active" exact={true}>
+          <div className="navbutton">
+            Home
+          </div>
+        </NavLink>
+        
+        <NavLink className="navbutton" to="/player/tyler1" activeClassName="is-active">
+            Players
+        </NavLink>
+
+        <NavLink className="navbutton" to="/team/cloud9" activeClassName="is-active">
+            Teams
+        </NavLink>
+
+        <NavLink className="navbutton" to="/leaderboard/kills" activeClassName="is-active">
+            Leaderboards
+        </NavLink>
+      </div>
     </div>
   </header>
 );
