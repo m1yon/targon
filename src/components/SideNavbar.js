a584ec5352998361d0b0b233d0ee802af69dfb0d
= <mlyon360@gmail.com>
2019-03-20 17:26:34 -0600

    navbutton updates

--- src/components/SideNavbar.js
+++ src/components/SideNavbar.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faUsers, faListOl, faCalendar, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faHome, faUser, faUsers, faListOl, faCalendar, faQuestionCircle);
+
+// Dashboard
+// Players
+// Teams
+// Leaderboards
+// Schedule
+// About
 
 const SideNavbar = () => (
   <div className="side-navbar">
@@ -8,11 +20,33 @@ const SideNavbar = () => (
     </div>
     <div className="navbar-button__container">
       <NavLink className="navbutton" to="/" activeClassName="is-active" exact={true}>
-          Home
+        <FontAwesomeIcon icon="home" />
+        <p>Home</p>
+      </NavLink>
+
+      <NavLink className="navbutton" to="/player/" activeClassName="is-active">
+        <FontAwesomeIcon icon="user" />
+        <p>Players</p>
+      </NavLink>
+
+      <NavLink className="navbutton" to="/teams/" activeClassName="is-active" exact={true}>
+        <FontAwesomeIcon icon="users" />
+        <p>Teams</p>
       </NavLink>
 
       <NavLink className="navbutton" to="/leaderboard/" activeClassName="is-active">
-        Leaderboard
+        <FontAwesomeIcon icon="list-ol" />
+        <p>Leaderboards</p>
+      </NavLink>
+
+      <NavLink className="navbutton" to="/schedule/" activeClassName="is-active" exact={true}>
+        <FontAwesomeIcon icon="calendar" />
+        <p>Schedule</p>
+      </NavLink>
+
+      <NavLink className="navbutton" to="/about/" activeClassName="is-active" exact={true}>
+        <FontAwesomeIcon icon="question-circle" />
+        <p>About</p>
       </NavLink>
     </div>
   </div>
