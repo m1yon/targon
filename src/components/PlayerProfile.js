a1c79489523dbcccace0ad13bbb3a0746739c0f6
= <mlyon360@gmail.com>
2019-03-24 15:41:34 -0600

    remade player-team-header

--- src/components/PlayerProfile.js
+++ src/components/PlayerProfile.js
@@ -2,26 +2,23 @@ import React from 'react';
 import { NavLink } from "react-router-dom";
 import PlayerStats from "./PlayerStats";
 
-const PlayerProfile = ({players, location}) => {
+const PlayerProfile = ({ players, location }) => {
   const playerName = location.pathname.slice(8);
 
   return (
     <div>
       <div className="team-banner">
-        <div className="team-banner--no-overflow">
-          <img src="/img/cloud9.jpg" className="team-banner__bg" />
-        </div>
-
-        <div className="team-banner--overflow">
+        <div className="team-banner__content">
+          {/* <img src="/img/cloud9.jpg" className="team-banner__bg" /> */}
           <img src="/img/tyler1.jpg" className="team-banner__profile-picture" />
           <div className="team-banner__player-info">
-            <h1>{ playerName }</h1>
+            <h1>{playerName}</h1>
             <h2>TYLER STEINKAMP</h2>
             <NavLink to={"/team/" + players[playerName].team}>{players[playerName].position} - {players[playerName].team}</NavLink>
           </div>
         </div>
       </div>
-      <PlayerStats player={ players[playerName] } />
+      <PlayerStats player={players[playerName]} />
     </div>
   );
 }
