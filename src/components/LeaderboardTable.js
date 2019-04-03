014c81f0efbd7890a35a98f443ce54f96a43d26c
= <mlyon360@gmail.com>
2019-04-03 13:50:37 -0600

    Player page and leaderboard page responsive updates

--- src/components/LeaderboardTable.js
+++ src/components/LeaderboardTable.js
@@ -13,83 +13,100 @@ const columns = [{
 }, {
   Header: <p className='table--header'>K</p>,
   accessor: "totalKills",
-  width: 30
+  width: 35
 }, {
   Header: <p className='table--header'>D</p>,
   accessor: "totalDeaths",
-  width: 30
+  width: 35
 }, {
   Header: <p className='table--header'>A</p>,
   accessor: "totalAssists",
-  width: 30
+  width: 35
 }, {
   Header: <p className='table--header'>KDA</p>,
-  accessor: "kda"
+  accessor: "kda",
+  width: 65,
 }, {
   Header: <p className='table--header'>KP</p>,
-  accessor: "kp"
+  accessor: "kp",
+  width: 65,
 }, {
   Header: <p className='table--header'>DTH%</p>,
-  accessor: "dthPercentage"
+  accessor: "dthPercentage",
+  width: 65,
 }, {
   Header: <p className='table--header'>FB%</p>,
-  accessor: "fbPercentage"
+  accessor: "fbPercentage",
+  width: 65,
 }, {
   Header: <p className='table--header'>GD10</p>,
-  accessor: "gd10"
+  accessor: "gd10",
+  width: 65,
 }, {
   Header: <p className='table--header'>XPD10</p>,
-  accessor: "xpd10"
+  accessor: "xpd10",
+  width: 65,
 }, {
   Header: <p className='table--header'>CSD10</p>,
-  accessor: "csd10"
+  accessor: "csd10",
+  width: 65,
 }, {
   Header: <p className='table--header'>CSPM</p>,
-  accessor: "cspm"
+  accessor: "cspm",
+  width: 65,
 }, {
   Header: <p className='table--header'>CS%P15</p>,
-  accessor: "csPercent15"
+  accessor: "csPercent15",
+  width: 65,
 }, {
   Header: <p className='table--header'>DPM</p>,
-  accessor: "dpm"
+  accessor: "dpm",
+  width: 65,
 }, {
   Header: <p className='table--header'>DMG%</p>,
-  accessor: "dmgPercentage"
+  accessor: "dmgPercentage",
+  width: 65,
 }, {
   Header: <p className='table--header'>EGPM</p>,
-  accessor: "earnedGoldPerMinute"
+  accessor: "earnedGoldPerMinute",
+  width: 65,
 }, {
   Header: <p className='table--header'>GOLD%</p>,
-  accessor: "goldPercentage"
+  accessor: "goldPercentage",
+  width: 65,
 }, {
   Header: <p className='table--header'>WPM</p>,
-  accessor: "wpm"
+  accessor: "wpm",
+  width: 65,
 }, {
   Header: <p className='table--header'>WCPM</p>,
-  accessor: "wcpm"
+  accessor: "wcpm",
+  width: 65,
 }
 ];
 
 const LeaderboardTable = ({ dsort, players }) => {
   return (
-    <ReactTable
-      data={players}
-      columns={columns}
-      className="-striped"
-      column={{
-        ...ReactTableDefaults.column,
-        minWidth: 25,
-        Cell: (props) => <p className="table--cell">{props.value}</p>,
-      }}
-      defaultPageSize={players.length}
-      defaultSorted={[{
-        id: dsort,
-        desc: true
-      }]}
-      defaultSortDesc={true}
-      showPagination={false}
-      resizable={false}
-    />
+    <div className="React-Table__container">
+      <ReactTable
+        data={players}
+        columns={columns}
+        className="-striped"
+        column={{
+          ...ReactTableDefaults.column,
+          minWidth: 25,
+          Cell: (props) => <p className="table--cell">{props.value}</p>,
+        }}
+        defaultPageSize={players.length}
+        defaultSorted={[{
+          id: dsort,
+          desc: true
+        }]}
+        defaultSortDesc={true}
+        showPagination={false}
+        resizable={false}
+      />
+    </div>
   );
 };
 
