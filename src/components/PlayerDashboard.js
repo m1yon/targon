c88a5f38439a3a75928132771bf9f118cbc5d041
= <mlyon360@gmail.com>
2019-04-05 10:06:23 -0600

    fixed leaderboard mobile styling

--- src/components/PlayerDashboard.js
+++ src/components/PlayerDashboard.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
+import { NavLink } from 'react-router-dom';
+import PlayerStats from './PlayerStats';
 import Loading from "./Loading";
-import PlayerProfile from './PlayerProfile';
 
 const PlayerDashboard = ({ players, location }) => {
   return (
@@ -12,6 +13,27 @@ const PlayerDashboard = ({ players, location }) => {
     </div>
   );
 };
+
+const PlayerProfile = ({ players, location }) => {
+  const playerName = location.pathname.slice(8);
+
+  return (
+    <div>
+      <div className="team-banner">
+        <div className="team-banner__content">
+          {/* <img src="/img/cloud9.jpg" className="team-banner__bg" /> */}
+          <img src="/img/tyler1.jpg" className="team-banner__profile-picture" />
+          <div className="team-banner__player-info">
+            <h1>{playerName}</h1>
+            <h2>TYLER STEINKAMP</h2>
+            <NavLink to={"/team/" + players[playerName].team}>{players[playerName].position} - {players[playerName].team}</NavLink>
+          </div>
+        </div>
+      </div>
+      <PlayerStats player={players[playerName]} />
+    </div>
+  );
+}
 
 const mapStateToProps = (state) => {
   return {
