c21967034b022b37455629aa7dc9a5b049528049
Michael Lyon <sevonefive@pop-os.localdomain>
2019-04-11 20:03:59 -0600

    TeamDashboard changes

--- src/components/TeamDashboard.js
+++ src/components/TeamDashboard.js
@@ -1,25 +1,18 @@
 import React from "react";
+import Loading from './Loading';
 
 const TeamDashboard = (props) => {
-  const teamName = props.location.pathname.slice(6);
-
-  return(
-    <div className="team-dashboard">
-      <div className="team-banner">
-        <div className="team-banner--no-overflow">
-          <img src="/img/cloud9.jpg" className="team-banner__bg" />
-        </div>
-
-        <div className="team-banner--overflow">
-          <div className="team-banner__player-info">
-            <h1>{teamName}</h1>
-            <h2>PLACEHOLDER</h2>
-            <p>PLACEHOLDER</p>
-          </div>
-        </div>
-      </div>
+  return (
+    <div className="player-dashboard">
+      <Loading component={
+        <TeamProfile />
+      } quickLoad={true}/>
     </div>
   );
 };
+
+const TeamProfile = () => (
+  <p>test</p>
+);
 
 export default TeamDashboard;
