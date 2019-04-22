5afca2719e1e26ac2b423ca3f38a475e406cb2ba
Michael Lyon <mlyon360@gmail.com>
2019-04-22 13:43:28 -0600

    fixed ._id being removed

--- server/helperMethods/arrayToObjects.js
+++ server/helperMethods/arrayToObjects.js
@@ -2,7 +2,7 @@ let arrayToObjects = ( (docs) =>{
     let returnedValue = {};
     for (let i = 0; i < docs.length; i++){
         returnedValue[docs[i]._id] = docs[i];
-        delete returnedValue[docs[i]._id]._id; // deletes the _id key to reduce redundency 
+        //delete returnedValue[docs[i]._id]._id; // deletes the _id key to reduce redundency 
       }
       return returnedValue;
 })
